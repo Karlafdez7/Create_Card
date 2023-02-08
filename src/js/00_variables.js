9d4204eb00b4628f08541a0d471118fd380ed43f
Cristina-F-R <cristina.faixedas@gmail.com>
2023-02-08 13:36:45 +0100

    add js collapsed

--- src/js/00_variables.js
+++ src/js/00_variables.js
@@ -1,3 +1,13 @@
 'use strict';
 
+const designLegend = document.querySelector ('.js-dropdown__design');
+const fillLegend = document.querySelector ('.js-dropdown__fill');
+const shareLegend = document.querySelector ('.js-dropdown__share');
+const designContent = document.querySelector ('.js-content__design');
+const fillContent = document.querySelector ('.js-content__fill');
+const shareContent = document.querySelector ('.js-content__share');
+const designArrow = document.querySelector ('.js-arrow__design');
+const fillArrow = document.querySelector ('.js-arrow__fill');
+const shareArrow = document.querySelector ('.js-arrow__share');
+
 
