10ddd4549c07ebe0f08fb9792fa3d039db7de190
Cristina-F-R <cristina.faixedas@gmail.com>
2023-02-28 12:35:31 +0100

    JS finished

--- src/js/03_share.js
+++ src/js/03_share.js
@@ -22,12 +22,23 @@ function handleClickSend(event) {
       console.log (data);
       if( ! data.success){
         // Mensaje error
-        msgShare.innerHTML = data.error; 
+        if(data.error.startsWith('Mandatory fields')){
+            msgShare.innerHTML = `<p class='error'> Faltan datos, revisa el formulario.</p>`;
+        }else if (data.error.startsWith('Database error')){
+            msgShare.innerHTML = `<p class='error'> Imagen demasiado grande.</p>`;
+        }else{
+            msgShare.innerHTML = `<p class='error'> Error. Intentalo de nuevo.</p>`;
+        }
+
       }else{
         // enlace de la tarjeta
-        msgShare.innerHTML = `<a href='${data.cardURL}' target='_blank'> Tu tarjeta </a>`;
+        msgShare.innerHTML = `<div class= 'share_card'> 
+         <h2 class='share_card_text'> La tarjeta ha sido creada: </h2> 
+            <a href='${data.cardURL}' target='_blank' class= 'share_card_cardURL'> Tu tarjeta </a> 
+        	<a class="share_card_twitter-share-button" href="https://twitter.com/intent/tweet?text=Mi%20tarjeta%20de%20presentaci%C3%B3n&url=${data.cardURL}"target='_blank' ><i class="fa-brands fa-twitter"></i> Compartir en Twitter</a>
+            </div>`;
 }
-} ); 
+} );
 }
 
 sendBTN.addEventListener( 'click', handleClickSend); 
