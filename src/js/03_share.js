1eff5bb8501674ba6db3a7cc5377746082ef93d1
laurapascual <laurapascualmateo@gmail.com>
2023-03-01 10:24:13 +0100

    Add css to share btn

--- src/js/03_share.js
+++ src/js/03_share.js
@@ -7,26 +7,26 @@ const msgShare = document.querySelector ('.js-msgShare');
 
 
 function handleClickSend(event) {
-    event.preventDefault(); 
+  event.preventDefault(); 
 
-    fetch('https://dev.adalab.es/api/card/', {
-        method:'POST',
-        headers: {
-            'content-type':'application/json',
-        },
-        body:JSON.stringify(data),
-      })
+  fetch('https://dev.adalab.es/api/card/', {
+    method:'POST',
+    headers: {
+      'content-type':'application/json',
+    },
+    body:JSON.stringify(data),
+  })
     .then ((response)=>response.json())
     .then ((data)=>{
 
       if( ! data.success){
         // Mensaje error
         if(data.error.startsWith('Mandatory fields')){
-            msgShare.innerHTML = `<p class='error'> Faltan datos, revisa el formulario.</p>`;
+          msgShare.innerHTML = `<p class='error'> Faltan datos, revisa el formulario.</p>`;
         }else if (data.error.startsWith('Database error')){
-            msgShare.innerHTML = `<p class='error'> Imagen demasiado grande.</p>`;
+          msgShare.innerHTML = `<p class='error'> Imagen demasiado grande.</p>`;
         }else{
-            msgShare.innerHTML = `<p class='error'> Error. Intentalo de nuevo.</p>`;
+          msgShare.innerHTML = `<p class='error'> Error. Intentalo de nuevo.</p>`;
         }
 
       }else{
@@ -36,6 +36,7 @@ function handleClickSend(event) {
             <a href='${data.cardURL}' target='_blank' class= 'share_card_cardURL'> Tu tarjeta </a> 
         	<a class="share_card_twitter-share-button" href="https://twitter.com/intent/tweet?text=Mi%20tarjeta%20de%20presentaci%C3%B3n&url=${data.cardURL}"target='_blank' ><i class="fa-brands fa-twitter"></i> Compartir en Twitter</a>
             </div>`;
+        sendBTN.classList.add('checked');
 }
 } );
 }
